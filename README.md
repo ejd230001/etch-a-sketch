f682177c98c5ff92bd9bf40afbc0a7ed72bb5c7c
Elden Deguia <eldendeguia@outlook.com>
2024-09-13 15:40:06 -0500

    Added random coloring and darkening

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # etch-a-sketch
+javascript project #2 :sunglasses:
+hover over squares to give them a random color,
+hover over them again to darken them
--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const head = document.querySelector("head");
+const container = document.querySelector(".container");
+
 const itemStyle = document.createElement("style");
 itemStyle.textContent = `
     .items {
@@ -6,29 +8,29 @@ itemStyle.textContent = `
     }`
 head.appendChild(itemStyle);
 
-for (let i = 0; i < 256; i++) {
-    const container = document.querySelector(".container");
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "items");
-    container.appendChild(newDiv);
-}
-
-const itemList = document.querySelectorAll(".items");
-itemList.forEach((item) => {
-    item.addEventListener("mouseover", (e) => {
-        item.classList.add("color");
-    })
-})
-
 const body = document.querySelector("body");
 const button = document.createElement("button");
 body.appendChild(button);
 button.textContent = "New";
 button.addEventListener("click", newGrid);
 
-function newGrid(event) {
-    let size = prompt("Size of new grid: ");
+for (let i = 0; i < 256; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "items");
+    container.appendChild(newDiv);
+}
 
+const itemList = document.querySelectorAll(".items");
+itemList.forEach(modifyColor)
+
+function newGrid(event) {
+    let size = prompt("Size of new grid (1-100): ");
+
+    if (size < 1 || size > 100) {
+        alert("Invalid Input");
+        return;
+    }
+    
     const currentItems = document.querySelectorAll(".items")
     currentItems.forEach((item) => item.remove());
     const oldStyle = document.querySelector("style");
@@ -44,14 +46,34 @@ function newGrid(event) {
     for (let i = 0; i < size * size; i++) {
         const newItemDiv = document.createElement("div");
         newItemDiv.classList.add("items");
-        const container = document.querySelector(".container");
         container.appendChild(newItemDiv);
     }
 
     const newItemList = document.querySelectorAll(".items");
-    newItemList.forEach((item) => {
-        item.addEventListener("mouseover", () => {
-            item.classList.add("color");
-        })
-    })
+    newItemList.forEach(modifyColor);
 }
+
+function modifyColor(item) {
+    item.addEventListener("mouseover", (e) => {
+        if (!item.hasAttribute("style")) {
+            item.style.backgroundColor = randomRGB();
+        }
+        else {
+            let currentColor = item.style.backgroundColor;
+            let currentOpacity = parseFloat(currentColor.split(',').pop().replace(')', ''));
+            if (currentOpacity < 1) {
+                let newOpacity = Math.min(currentOpacity + .1, 1);
+                console.log(currentColor.substring(0, currentColor.length - 4) + newOpacity + ")");
+                item.style.backgroundColor = currentColor.substring(0, currentColor.length - 4) + newOpacity + ")";
+            }
+        }
+    });
+}
+
+function randomRGB() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgba(${r}, ${g}, ${b}, .1)`
+}
+
